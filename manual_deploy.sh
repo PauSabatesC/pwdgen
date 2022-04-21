586fb77003d47ed6e5d0f16326e100d31bd6123c
PauSabatesC <camposabates@gmail.com>
2022-04-21 19:54:09 +0200

    Added github action

--- manual_deploy.sh
+++ manual_deploy.sh
@@ -6,11 +6,15 @@ if [[ -z $version ]]; then
   echo "ERROR: specify a version e.g v0.1.1"
 fi
 
-# Remember to push the commit first then:
+echo "Did you push the local commit?"
+echo "Did you export your github token? export GITHUB_TOKEN=\"YOUR_GH_TOKEN\""
+echo "Do nothing if you did all... (ctrl+c to abort)"
+sleep 15
 
 git tag -a $version -m "Version $version"
 git push origin $version
 
-#Remember to export github token: export GITHUB_TOKEN="YOUR_GH_TOKEN"
+echo "Tag created and pushed. Doing the release in 10 seconds..."
+sleep 10
 goreleaser release --rm-dist
 
