7e6f0a5252febe015ad8ed4897aa65b023189a57
PauSabatesC <camposabates@gmail.com>
2022-04-21 22:14:53 +0200

    Added revert_tag script

---
+++ revert_tag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+version=$1 #e.g. v0.1.1
+
+if [[ -z $version ]]; then
+  echo "ERROR: specify a version e.g v0.1.1"
+  exit 1
+fi
+
+git tag -d $version
+git push --delete origin $versiona
+echo "Done. Tag $version is deleted, you can push it again"
+
